02b4a676dc390e5da4841359e7466b5f3ad3f67f
Aman Agarwal <aaman4901@gmail.com>
2023-05-04 17:08:52 +0530

    removed redundant code

--- app.js
+++ app.js
@@ -6,20 +6,6 @@ dotenv.config();
 const getSqlConnection = require('./services/sequelize.js');
 global.DATABASE = getSqlConnection();
 
-const config = require('./config/config.js');
-const express = require('express');
-const app = express();
-const cors = require('cors');
-
-// Allowing cross origin access
-app.use(cors());
-
-app.use(express.json());
-
-app.listen(config.SERVER_PORT, () => {
-  console.log(`Server is listening at ${config.SERVER_PORT}`);
-});
-
 // Running task
 const task = require('./services/task.js');
 task.uploadImages();
